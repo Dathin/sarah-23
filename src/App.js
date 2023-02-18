99bd9357c32b192205e7420f4350b8f592c58050
Pedro Caires <pedro.caires@mheducation.com>
2023-02-17 23:12:06 -0300

    x

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './photo.jpg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Parabéns Sarah!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          🐢 ❤️ 🐎
+        </p>
       </header>
     </div>
   );
